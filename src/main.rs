b9483e9254ec4255785d9eec369cdaa2608069a1
Vladas Zakrevskis <146100@gmail.com>
2021-03-23 23:06:41 +0300

    bororow

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,42 @@ mod gl_wrapper;
 use crate::gm::*;
 use crate::gl_wrapper::GLWrapper;
 
+struct Parent<'a> {
+    pub parent: Option<&'a Parent<'a>>,
+    pub childred: Vec<Parent<'a >>
+}
+
+
+impl<'a> Parent<'a> {
+
+    pub fn new() -> Parent<'a> {
+        Parent { parent: None, childred: vec![] }
+    }
+
+    pub fn add_child(&mut self, child: Parent) {
+        // let mut ch = Parent { parent: None, childred: vec![] };
+        // ch.parent = Some(self);
+        // self.childred.push(ch)
+    }
+}
+
 fn main() {
+
+    // let mut items = vec![1];
+    // let mut item = items.last();
+    //
+    // items.push(2);
+    //
+    // log!(&mut item);
+
+
+    // let mut root = Parent::new();
+    //
+    // let mut child = Parent::new();
+    //
+    // child.parent = Some(&root);
+    // root.childred.push(child);
+
+
     GLWrapper::init(Size { width: 500.0, height: 500.0 });
 }
