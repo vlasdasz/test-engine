f2f74e3fdfb1d502cd17d1efe7c849d7dfb1244a
Vladas Zakrevskis <146100@gmail.com>
2021-03-31 14:44:06 +0300

    sokol

--- src/image/image.rs
+++ src/image/image.rs
@@ -5,11 +5,10 @@ use std::ffi::c_void;
 use crate::gm::Size;
 use crate::gl_wrapper::{TextureLoader, GLWrapper};
 use std::path::PathBuf;
-use assimp::Texture;
 use image::io::Reader;
 use image::{load, GenericImageView};
 use image::DynamicImage::*;
-use soil2::SOIL_load_image;
+use soil2::{SOIL_load_image, SOIL_free_image_data};
 use std::os::raw::c_int;
 
 #[derive(Debug, Copy, Clone)]
@@ -27,40 +26,32 @@ impl Image {
 
     pub fn load(path: &PathBuf) -> Image {
 
-        //
-        // log!(path);
-        // //
-        // // filename: *const c_char,
-        // // width: *mut c_int,
-        // // height: *mut c_int,
-        // // channels: *mut c_int,
-        // // force_channels: c_int
-        //
-        // unsafe {
-        //
-        //     let mut width: c_int = -1;
-        //     let mut height: c_int = -1;
-        //     let mut channels: c_int = -1;
-        //
-        //
-        //     let data = SOIL_load_image(
-        //         path.to_str().unwrap().as_ptr() as *const i8,
-        //         &mut width,
-        //         &mut height,
-        //         &mut channels,
-        //         0
-        //     );
-        //
-        //     let image = Image::from(
-        //         data as *const c_void,
-        //         Size { width: width as f32, height: height as f32 },
-        //         channels as u32
-        //     );
-        //
-        //     image
+        log!(path);
 
-        Image::new()
+        unsafe {
 
+            let mut width: c_int = -1;
+            let mut height: c_int = -1;
+            let mut channels: c_int = -1;
+
+            let data = SOIL_load_image(
+                path.to_str().unwrap().as_ptr() as *const i8,
+                &mut width,
+                &mut height,
+                &mut channels,
+                0
+            );
+
+            let image = Image::from(
+                data as *const c_void,
+                Size { width: width as f32, height: height as f32 },
+                channels as u32
+            );
+
+            SOIL_free_image_data(data);
+
+            image
+        }
     }
 
     pub fn from(data: *const c_void, size: Size, channels: u32) -> Image {
