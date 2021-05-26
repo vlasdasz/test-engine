3bd2f5f1b7c3be0d6a2c70cc7756bf629f575223
Vladas Zakrevskis <146100@gmail.com>
2021-05-27 02:23:13 +0300

    box tweaking

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,34 @@ mod gl_wrapper;
 use crate::gl_wrapper::GLDrawer;
 use crate::gm::Size;
 use crate::te::Screen;
+use std::fmt::Debug;
+use tools::HasNew;
+
+trait Object where Self: Debug {
+    fn get_obj(&self) -> &BaseObject;
+}
+
+#[derive(Debug)]
+struct BaseObject {
+    pub i: u32,
+    pub sub: Vec<Box<dyn Object>>
+}
+
+impl HasNew for BaseObject {
+    fn new() -> BaseObject {
+        BaseObject {
+            i: 15,
+            sub: vec![]
+        }
+    }
+}
+
+impl Object for BaseObject {
+    fn get_obj(&self) -> &BaseObject {
+        &self
+    }
+}
+
 
 fn main() {
     GLDrawer::<Screen>::with_size(Size {
