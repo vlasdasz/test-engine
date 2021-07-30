171dd91383610d00e687a5174c006c339c788011
Vladas Zakrevskis <146100@gmail.com>
2021-07-30 22:33:14 +0300

    mokro

--- test/src/main.rs
+++ test/src/main.rs
@@ -1,6 +1,17 @@
 use gl_wrapper::GLDrawer;
 use test_engine::TestScreen;
 
+extern crate proc_macro;
+use proc_macro::make_answer;
+
+make_answer!();
+
+
+
 fn main() {
+    dbg!(answer());
+
+
+    return;
     GLDrawer::<TestScreen>::with_size((1200, 600).into()).start_main_loop();
 }
