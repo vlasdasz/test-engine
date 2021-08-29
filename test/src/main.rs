19adf963076c2ea0299de7cd36fdbf377d7a7b93
Vladas Zakrevksis <146100@gmail.com>
2021-08-29 05:15:02 +0300

    own firs implementation

--- test/src/main.rs
+++ test/src/main.rs
@@ -1,18 +1,81 @@
-use gl_wrapper::GLDrawer;
-use test_engine::TestScreen;
+use gm::Rect;
+use std::alloc::alloc;
+use std::alloc::Layout;
+use std::fmt::{Debug, Formatter};
+use std::ops::{Deref, DerefMut};
+use std::ptr::{null, null_mut};
 
-extern crate proc_macro;
-extern crate tools;
-use proc_macro::AsAny;
+extern crate gm;
 
-#[derive(AsAny)]
-struct Spesogon {}
+struct Own<T: Debug> {
+    pointer: *mut T,
+}
+
+impl<T: Debug> Own<T> {
+    pub fn from(value: T) -> Self {
+        Self {
+            pointer: unsafe {
+                let ptr = alloc(Layout::new::<T>()) as *mut T;
+                *ptr = value;
+                ptr
+            },
+        }
+    }
+}
+
+impl<T: Debug> Deref for Own<T> {
+    type Target = T;
+    fn deref(&self) -> &T {
+        unsafe { &*self.pointer }
+    }
+}
+
+impl<T: Debug> DerefMut for Own<T> {
+    fn deref_mut(&mut self) -> &mut T {
+        unsafe { &mut *self.pointer }
+    }
+}
+
+impl<T: Debug> Debug for Own<T> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        self.deref().fmt(f)
+    }
+}
+
+struct Fer<T> {
+    pointer: *mut T,
+}
+
+// impl<T> Fer<T> {
+//     pub fn from()
+// }
+
+struct View {
+    pub superview: *const View,
+    pub subviews: Vec<Box<View>>,
+}
+
+impl View {
+    pub fn new() -> Self {
+        Self {
+            superview: null(),
+            subviews: vec![],
+        }
+    }
+
+    //pub fn add_subview()
+}
 
 fn main() {
-    let spes = Spesogon {};
+    let sok: Rect = (1, 2, 3, 4).into();
 
-    drop(spes);
+    dbg!(&sok);
 
-    //return;
-    GLDrawer::<TestScreen>::with_size((1200, 600).into()).start_main_loop();
+    let mut own = Own::from(sok);
+
+    dbg!(&own);
+
+    own.origin.x += 20.0;
+
+    dbg!(&own);
 }
