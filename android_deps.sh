b510b6bb80e04f6f75f3c2cecc6d4d5c3d4f8c62
Vladas Zakrevskis <146100@gmail.com>
2022-02-04 01:14:24 +0200

    ...

--- android_deps.sh
+++ android_deps.sh
@@ -1,2 +1,6 @@
 brew install --cask android-sdk
-export ANDROID_HOME=$HOME/Library/Android/sdk/
+
+rustup target add aarch64-linux-android armv7-linux-androideabi i686-linux-android
+
+export ANDROID_HOME="/opt/homebrew/share/android-ndk"
+export ANDROID_NDK_HOME="/opt/homebrew/share/android-ndk"
---
+++ build.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import shutil
+import platform
+
+is_windows = platform.system() == "Windows"
+is_mac     = platform.system() == "Darwin"
+is_linux   = platform.system() == "Linux"
+
+unix = is_mac or is_linux
+
+ios = False
+android = False
+
+if len(sys.argv) > 1:
+    if sys.argv[1] == "ios":
+        ios = True
+    if sys.argv[1] == "android":
+        android = True
+
+
+def _get_home():
+    if "HOME" in os.environ:
+        return os.environ["HOME"]
+    return os.path.expanduser("~")
+    
+
+home = _get_home()
+
+this_path = os.path.dirname(os.path.abspath(__file__))
+
+
+def run(string):
+    print(string)
+    if os.system(string):
+        raise Exception("Shell script has failed")
+
+
+def ndk_home():
+    if "NDK_HOME" in os.environ:
+        return os.environ["NDK_HOME"]
+    return "${ANDROID_HOME}/ndk/22.1.7171670"
+
+
+def setup_android():
+    if os.path.isdir("NDK"):
+        return
+    run("mkdir NDK")
+    run("rustup target add aarch64-linux-android armv7-linux-androideabi i686-linux-android")
+    ndk = ndk_home()
+    if is_windows:
+        run("py " + ndk + "/build/tools/make_standalone_toolchain.py --api 21 --arch arm64 --install-dir NDK/arm64")
+        run("py " + ndk + "/build/tools/make_standalone_toolchain.py --api 19 --arch arm --install-dir NDK/arm")
+        run("py " + ndk + "/build/tools/make_standalone_toolchain.py --api 19 --arch x86 --install-dir NDK/x86")
+    else:
+        run(ndk + "/build/tools/make_standalone_toolchain.py --api 21 --arch arm64 --install-dir NDK/arm64")
+        run(ndk + "/build/tools/make_standalone_toolchain.py --api 19 --arch arm --install-dir NDK/arm")
+        run(ndk + "/build/tools/make_standalone_toolchain.py --api 19 --arch x86 --install-dir NDK/x86")
+
+
+if android:
+    setup_android()
+
+
+print("Arch:")
+print(platform.uname())
+
+
+if is_linux:
+    print("Lin setup")
+    run("sudo apt update")
+    run("sudo apt -y install cmake mesa-common-dev libgl1-mesa-dev libglu1-mesa-dev xorg-dev")
+
+
+if ios:
+    os.environ["CARGO_CFG_TARGET_OS"] = "ios"
+    run("rustup target add aarch64-apple-ios x86_64-apple-ios ")
+    run("cargo install cargo-lipo")
+    run("cargo lipo --release")
+    os.chdir("mobile/iOS")
+    run("xcodebuild -showsdks")
+    run("xcodebuild -sdk iphonesimulator -scheme TestEngine build")
+elif android:
+    os.environ["CARGO_CFG_TARGET_OS"] = "android"
+    run("cargo build --target aarch64-linux-android --release --lib")
+    run("cargo build --target armv7-linux-androideabi --release --lib")
+    run("cargo build --target i686-linux-android --release --lib")
+
+    run("mkdir -p mobile/android/app/src/main/jniLibs/")
+    run("mkdir -p mobile/android/app/src/main/jniLibs/arm64-v8a")
+    run("mkdir -p mobile/android/app/src/main/jniLibs/armeabi-v7a")
+    run("mkdir -p mobile/android/app/src/main/jniLibs/x86")
+
+    try:
+        os.symlink(this_path + "/target/aarch64-linux-android/release/libtest_game.so", "mobile/android/app/src/main/jniLibs/arm64-v8a/libtest_game.so")
+        os.symlink(this_path + "/target/armv7-linux-androideabi/release/libtest_game.so", "mobile/android/app/src/main/jniLibs/armeabi-v7a/libtest_game.so")
+        os.symlink(this_path + "/target/i686-linux-android/release/libtest_game.so", "mobile/android/app/src/main/jniLibs/x86/libtest_game.so")
+    except FileExistsError:
+        print("exists")
+else:
+    os.environ["CARGO_CFG_TARGET_OS"] = "desktop"
+    run("cargo build")
