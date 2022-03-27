0a1cc6d5cc889a0b12d7864ab570c0bc610b11ea
Vladas Zakrevskis <146100@gmail.com>
2022-03-27 16:21:46 +0300

    app with tokio

--- test_engine/src/app.rs
+++ test_engine/src/app.rs
@@ -5,44 +5,52 @@ use std::{
 };
 
 use gl_wrapper::monitor::Monitor;
+use tokio::runtime::Runtime;
 use ui::{input::touch::Event, Touch};
 
 use crate::{game_view::GameView, Screen};
 
-#[derive(Default)]
 pub struct App<T> {
     pub screen:  Option<Screen>,
     pub monitor: Monitor,
+    runtime:     Runtime,
     _view:       PhantomData<T>,
 }
 
 impl<T: GameView + 'static> App<T> {
     pub fn create_screen(&mut self, width: c_int, height: c_int) {
-        let mut screen = Screen::new((width, height).into());
+        self.runtime.block_on(async {
+            let mut screen = Screen::new((width, height).into());
 
-        screen.ui.set_view(T::boxed());
-        screen.ui.add_debug_view();
+            screen.ui.set_view(T::boxed());
+            screen.ui.add_debug_view();
 
-        screen.add_monitor(self.monitor.clone());
+            screen.add_monitor(self.monitor.clone());
 
-        self.screen = screen.into();
+            self.screen = screen.into();
+        });
     }
 
     pub fn set_screen_size(&mut self, width: c_int, height: c_int) {
-        self.screen.as_mut().unwrap().set_size((width, height).into());
+        self.runtime.block_on(async {
+            self.screen.as_mut().unwrap().set_size((width, height).into());
+        });
     }
 
     pub fn update_screen(&mut self) {
-        self.screen.as_mut().unwrap().update()
+        self.runtime
+            .block_on(async { self.screen.as_mut().unwrap().update() });
     }
 
     pub fn on_touch(&mut self, id: c_ulong, x: c_float, y: c_float, event: c_int) {
         #[allow(clippy::useless_conversion)]
-        self.screen.as_mut().unwrap().ui.on_touch(Touch {
-            id:       id.into(),
-            position: (x, y).into(),
-            event:    Event::from_int(event),
-        })
+        self.runtime.block_on(async {
+            self.screen.as_mut().unwrap().ui.on_touch(Touch {
+                id:       id.into(),
+                position: (x, y).into(),
+                event:    Event::from_int(event),
+            })
+        });
     }
 
     #[allow(clippy::too_many_arguments)]
@@ -71,5 +79,16 @@ impl<T: GameView + 'static> App<T> {
         dbg!(&monitor);
 
         self.monitor = monitor;
+    }
+}
+
+impl<T: GameView> Default for App<T> {
+    fn default() -> Self {
+        Self {
+            screen:  Default::default(),
+            monitor: Default::default(),
+            runtime: tokio::runtime::Runtime::new().unwrap(),
+            _view:   Default::default(),
+        }
     }
 }
