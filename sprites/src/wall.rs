08f01b1cd386bac1ea4c921bb79895ad08b9b4f4
Vladas Zakrevskis <146100@gmail.com>
2022-04-03 02:29:11 +0300

    animated walls

--- sprites/src/wall.rs
+++ sprites/src/wall.rs
@@ -1,8 +1,9 @@
+use gm::Point;
 use rapier2d::{
     na::Vector2,
-    prelude::{ColliderBuilder, ColliderHandle},
+    prelude::{Collider, ColliderBuilder, ColliderHandle},
 };
-use rtools::{Rglica, ToRglica};
+use rtools::{IntoF32, Rglica, ToRglica};
 
 use crate::{Level, Sprite, SpriteBase};
 
@@ -24,9 +25,45 @@ impl Wall {
         level.level_mut().sprites.push(boxed);
         wall
     }
+
+    pub fn collider(&self) -> &Collider {
+        &self.level().colliders()[self.handle]
+    }
+
+    pub fn collider_mut(&mut self) -> &mut Collider {
+        let handle = self.handle;
+        &mut self.level_mut().colliders_mut()[handle]
+    }
+
+    pub fn set_x(&mut self, x: impl IntoF32) {
+        let mut pos = self.position();
+        pos.x = x.into_f32();
+        self.set_position(pos);
+    }
+
+    pub fn set_y(&mut self, y: impl IntoF32) {
+        let mut pos = self.position();
+        pos.y = y.into_f32();
+        self.set_position(pos);
+    }
+
+    pub fn set_position(&mut self, pos: Point) {
+        self.collider_mut().set_position([pos.x, pos.y].into())
+    }
 }
 
 impl Sprite for Wall {
+    fn update(&mut self) {
+        let mut this = self.to_rglica();
+        this.sprite_mut().position = self.position();
+        this.sprite_mut().rotation = self.rotation();
+    }
+
+    fn position(&self) -> Point {
+        let pos = self.collider().position().translation;
+        (pos.x, pos.y).into()
+    }
+
     fn collider_handle(&self) -> Option<ColliderHandle> {
         self.handle.into()
     }
--- test_game/src/benchmark/benchmark_level.rs
+++ test_game/src/benchmark/benchmark_level.rs
@@ -1,8 +1,19 @@
-use test_engine::{assets::Assets, sprites::Player, Level, LevelBase, Sprite};
+use rtools::{Animation, Rglica};
+use test_engine::{
+    assets::Assets,
+    sprites::{Player, Wall},
+    Level, LevelBase, Sprite,
+};
 
 #[derive(Debug, Default)]
 pub struct BenchmarkLevel {
     level:             LevelBase,
+    left_wall:         Rglica<Wall>,
+    right_wall:        Rglica<Wall>,
+    floor:             Rglica<Wall>,
+    left_animation:    Animation,
+    right_animation:   Animation,
+    floor_animation:   Animation,
     pub bullets_count: u64,
 }
 
@@ -10,9 +21,18 @@ impl BenchmarkLevel {
     fn make_walls(&mut self) {
         let square = Assets::image("square.png");
 
-        self.add_wall((0, 0, 100, 1).into()).set_image(square.clone());
-        self.add_wall((40, 0, 1, 100).into()).set_image(square.clone());
-        self.add_wall((-40, 0, 1, 100).into()).set_image(square);
+        self.floor = self.add_wall((0, 0, 100, 4).into());
+        self.floor.set_image(square.clone());
+
+        self.left_wall = self.add_wall((-40, 0, 10, 100).into());
+        self.left_wall.set_image(square.clone());
+
+        self.right_wall = self.add_wall((40, 0, 10, 100).into());
+        self.right_wall.set_image(square);
+
+        self.left_animation = Animation::new(-50, -40, 5);
+        self.right_animation = Animation::new(50, 40, 5);
+        self.floor_animation = Animation::new(-10, 0, 4);
     }
 }
 
@@ -22,13 +42,16 @@ impl Level for BenchmarkLevel {
         self.level.player.weapon.set_image(Assets::image("ak.png"));
         self.level.player.weapon.bullet_image = Assets::image("bullet.png").into();
         self.level.player.weapon.bullet_speed = 100.0;
-        self.set_scale(0.8);
+        self.set_scale(1.2);
         self.make_walls();
     }
 
     fn update(&mut self) {
         self.player().weapon.shoot_at((10, 5).into());
         self.bullets_count += 1;
+        self.left_wall.set_x(self.left_animation.value());
+        self.right_wall.set_x(self.right_animation.value());
+        // self.floor.set_y(self.floor_animation.value());
     }
 
     fn level(&self) -> &LevelBase {
