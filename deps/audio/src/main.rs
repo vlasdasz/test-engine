6e6ac1753d68aabbab22453f4df68629977ad8aa
Vladas Zakrevskis <146100@gmail.com>
2022-04-11 00:44:48 +0300

    storing sound buffer

--- deps/audio/src/main.rs
+++ deps/audio/src/main.rs
@@ -4,9 +4,8 @@ use audio::Sound;
 use rtools::data_manager::LoadFromPath;
 
 fn main() {
-    let sound = Sound::load(Path::new(
+    let _sound = Sound::load(Path::new(
         "/Users/vladas/Downloads/Electrochok - Adam Park.mp3",
     ));
-    sound.play();
     std::thread::sleep(std::time::Duration::from_secs(5));
 }
--- deps/audio/src/sound.rs
+++ deps/audio/src/sound.rs
@@ -1,21 +1,25 @@
 use std::{
     fs::File,
-    io::BufReader,
+    io::{Cursor, Read},
     path::{Path, PathBuf},
 };
 
-use rodio::{OutputStream, OutputStreamHandle, Sink};
-use rtools::{data_manager::LoadFromPath};
+use rodio::{Decoder, OutputStream, OutputStreamHandle, Sink};
+use rtools::data_manager::LoadFromPath;
 
 pub struct Sound {
     _path:          PathBuf,
     _stream:        OutputStream,
     _stream_handle: OutputStreamHandle,
-    sink:          Sink,
+    data:           Vec<u8>,
+    sink:           Sink,
 }
 
 impl Sound {
     pub fn play(&self) {
+        let cursor = Cursor::new(self.data.clone());
+        let input = Decoder::new(cursor).unwrap();
+        self.sink.append(input);
         self.sink.play()
     }
 }
@@ -23,14 +27,19 @@ impl Sound {
 impl LoadFromPath for Sound {
     fn load(path: &Path) -> Self {
         let (_stream, _stream_handle) = OutputStream::try_default().unwrap();
-        let file = BufReader::new(File::open(path).unwrap());
-        let sink = _stream_handle.play_once(file).unwrap();
-        sink.pause();
+
+        let mut file = File::open(path).unwrap();
+        let mut data = Vec::new();
+        file.read_to_end(&mut data).unwrap();
+
+        let cursor = Cursor::new(data.clone());
+        let sink = _stream_handle.play_once(cursor).unwrap();
 
         Self {
             _path: path.to_path_buf(),
             _stream,
             _stream_handle,
+            data,
             sink,
         }
     }
