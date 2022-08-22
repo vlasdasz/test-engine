7f1bdfcf957a6225e585b0ee27a775621c31219e
Vladas Zakrevskis <146100@gmail.com>
2022-08-23 00:12:22 +0300

    order

--- deps/ui/src/layout/mod.rs
+++ deps/ui/src/layout/mod.rs
@@ -4,5 +4,5 @@ mod placer;
 mod tiling_rule;
 
 pub use new_placer::NewPlacer;
-pub use tiling_rule::TilingRule;
 pub use placer::{place_vertically, Anchor, Placer};
+pub use tiling_rule::TilingRule;
