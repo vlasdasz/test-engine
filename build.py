6f0fccd66504964a4838d35945ac7a3853b7a932
Vladas <146100@gmail.com>
2022-09-21 21:29:34 +0300

    path

--- build.py
+++ build.py
@@ -59,6 +59,7 @@ this_path = os.path.dirname(os.path.abspath(__file__))
 
 if unix:
     run("echo $0")
+    run("echo $PATH")
     run("curl https://sh.rustup.rs -sSf | sh -s -- -y")
     run("echo \"$HOME/.cargo/env\"")
     run("echo $HOME")
