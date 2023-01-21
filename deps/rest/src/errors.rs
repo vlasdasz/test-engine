80e0f2bdd09277ab0187528bfe97c43402cee3d8
Vladas Zakrevskis <146100@gmail.com>
2023-01-21 23:04:43 +0200

    rest error

--- deps/rest/src/errors.rs
+++ deps/rest/src/errors.rs
@@ -1,36 +1,48 @@
-use std::fmt::{Display, Formatter};
+use std::{
+    error::Error,
+    fmt::{Display, Formatter},
+};
 
-#[derive(Debug)]
-pub struct Error(String);
+use actix_web::ResponseError;
+use serde::{Deserialize, Serialize};
 
-impl From<String> for Error {
-    fn from(string: String) -> Self {
-        Error(string)
+#[derive(Debug, Eq, PartialEq, Serialize, Deserialize)]
+pub struct RestError {
+    message: String,
+}
+
+impl Error for RestError {}
+
+impl From<String> for RestError {
+    fn from(message: String) -> Self {
+        Self { message }
     }
 }
 
-impl From<&'static str> for Error {
+impl From<&'static str> for RestError {
     fn from(str: &'static str) -> Self {
-        Error(str.into())
+        Self { message: str.into() }
     }
 }
 
-impl From<reqwest::Error> for Error {
+impl From<reqwest::Error> for RestError {
     fn from(error: reqwest::Error) -> Self {
         format!("Network error: {error}").into()
     }
 }
 
-impl From<serde_json::Error> for Error {
+impl From<serde_json::Error> for RestError {
     fn from(err: serde_json::Error) -> Self {
         format!("Serialization error: {err}").into()
     }
 }
 
-impl Display for Error {
+impl Display for RestError {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "Network error: {}", self.0)
+        write!(f, "{}", serde_json::to_string(self).unwrap())
     }
 }
 
-pub type NetResult<T> = Result<T, Error>;
+pub type NetResult<T> = Result<T, RestError>;
+
+impl ResponseError for RestError {}
--- deps/rest/src/lib.rs
+++ deps/rest/src/lib.rs
@@ -7,6 +7,6 @@ mod request;
 mod response;
 
 pub use api::API;
-pub use errors::{Error, NetResult};
+pub use errors::{NetResult, RestError, *};
 pub use method::Method;
 pub use request::*;
