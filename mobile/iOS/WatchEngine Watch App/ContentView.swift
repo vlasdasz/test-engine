91faf13e92af6cb7a1035a7af9a1c69eb1feeb92
Vladas Zakrevskis <146100@gmail.com>
2023-05-29 20:29:00 +0300

    empty watch app

---
+++ mobile/iOS/WatchEngine Watch App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  WatchEngine Watch App
+//
+//  Created by Vladas Zakrevskis on 2023-05-29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ mobile/iOS/WatchEngine Watch App/WatchEngineApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchEngineApp.swift
+//  WatchEngine Watch App
+//
+//  Created by Vladas Zakrevskis on 2023-05-29.
+//
+
+import SwiftUI
+
+@main
+struct WatchEngine_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
