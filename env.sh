c02469b7b0efcd2602400398f4c5a53c47832cea
Vladas <146100@gmail.com>
2023-08-14 16:37:45 +0300

    working make fly

--- env.sh
+++ env.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-export ANDROID_LIB_NAME=test_game
-export IOS_PROJECT_NAME=TestEngine
+export ANDROID_LIB_NAME="test_game"
+export IOS_PROJECT_NAME="TestEngine"
+export PROJECT_NAME="TestEngine"
+export SCHEME_NAME="TestEngine Release"
