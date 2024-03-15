73ae02f3d99104c0d2ffa5cbb7cd21630d191ccc
Vladas Zakrevskis <146100@gmail.com>
2024-03-15 02:53:39 +0200

    ios callbacks in separate file

--- mobile/iOS/TestEngine/main.m
+++ mobile/iOS/TestEngine/main.m
@@ -5,41 +5,10 @@
 //  Created by Vladas Zakrevskis on 02.04.2021.
 //
 
-
-#import <Foundation/Foundation.h>
-#import <UIKit/UIKit.h>
+#include "test_engine.h"
 
 int test_game(void);
 
 int main(int argc, char * argv[]) {
     return test_game();
-}
-
-UITextField* text_field = nil;
-
-void ios_init_text_field(void) {
-    NSLog(@"ios_init_text_field");
-
-    assert(text_field == nil);
-    UIViewController *controller = [UIApplication sharedApplication].keyWindow.rootViewController;
-    text_field = [UITextField new];
-    text_field.textAlignment = NSTextAlignmentCenter;
-    [controller.view addSubview:text_field];
-    
-    NSLog(@"UITextField initialized");
-}
-
-void ios_open_keyboard(float x, float y, float width, float height) {
-    CGFloat scale = [[UIScreen mainScreen] scale];
-    [text_field setFrame:CGRectMake(x / scale, y / scale, width / scale, height / scale)];
-    [text_field setHidden:NO];
-    NSLog(@"ios_open_keyboard");
-    [text_field becomeFirstResponder];
-}
-
-const char* ios_close_keyboard(void) {
-    NSLog(@"ios_close_keyboard");
-    [text_field resignFirstResponder];
-    [text_field setHidden:YES];
-    return [text_field.text UTF8String];
 }
