6b9bcdefd8bdfcba102cf62ac4d96e3ccb8f3d70
Vladas <146100@gmail.com>
2024-03-27 11:57:10 +0200

    removed test_engine.h, updated toolchain

--- test_game/src/test_game.rs
+++ test_game/src/test_game.rs
@@ -4,10 +4,9 @@ use crate::interface::test_game_view::TestGameView;
 
 pub fn start_test_game() {
     let runtime = tokio::runtime::Runtime::new().unwrap();
-    runtime
-        .block_on(async {
-            #[cfg(mobile)]
-            test_engine::refs::set_current_thread_as_main();
-            App::start(TestGameView::new()).await.unwrap()
-        });
+    runtime.block_on(async {
+        #[cfg(mobile)]
+        test_engine::refs::set_current_thread_as_main();
+        App::start(TestGameView::new()).await.unwrap()
+    });
 }
