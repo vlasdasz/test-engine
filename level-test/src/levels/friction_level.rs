ce5f49267c4519f78b9048f38d3d96b2af9d5e92
Vladas <146100@gmail.com>
2024-06-02 17:23:36 +0300

    a

--- level-test/src/levels/friction_level.rs
+++ level-test/src/levels/friction_level.rs
@@ -1,5 +1,9 @@
-use test_engine::level::level;
+use test_engine::level::{level, LevelSetup};
 
 #[level]
 #[derive(Default)]
 pub struct FrictionLevel {}
+
+impl LevelSetup for FrictionLevel {
+    fn setup(&mut self) {}
+}
