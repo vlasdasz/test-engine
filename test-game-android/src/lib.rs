cb9861a21a160d8d426b9ac864b14d96f66c6cb3
Vladas Zakrevskis <146100@gmail.com>
2024-07-21 23:56:45 +0300

    hello from jni

--- test-game-android/src/lib.rs
+++ test-game-android/src/lib.rs
@@ -4,18 +4,27 @@ fn android_main(app: test_game::AndroidApp) {
     test_game::start_test_game(app);
 }
 
-use jni::{objects::JString, JNIEnv};
+use jni::{
+    objects::{JClass, JString},
+    JNIEnv,
+};
 
+// Works
 #[no_mangle]
-pub extern "system" fn Java_com_example_test_1game_MainActivity_helloFromJNI<'local>(
+pub unsafe extern "C" fn Java_com_example_test_1game_MainActivity_helloFromJNI<'local>(
     mut env: JNIEnv<'local>,
+    _: JClass,
     input: JString<'local>,
 ) {
+    dbg!("Java_com_example_test_1game_MainActivity_helloFromJNI");
+
     // First, we have to get the string out of Java. Check out the `strings`
     // module for more info on how this works.
     let input: String = env.get_string(&input).expect("Couldn't get java string!").into();
 
     dbg!(&input);
+
+    // Then we have to create a new Java string to return. Again, more inf
 
     // Then we have to create a new Java string to return. Again, more info
     // in the `strings` module.
