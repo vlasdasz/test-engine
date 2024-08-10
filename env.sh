c7257cbd629cc1f80861b1b39964e1d626a08aa6
Vladas Zakrevskis <146100@gmail.com>
2024-08-10 21:51:03 +0300

    fixed ios project name

--- env.sh
+++ env.sh
@@ -1,6 +1,4 @@
 export ANDROID_LIB_NAME="test-game"
-export IOS_SIMULATOR_SCHEME="TestGame Simulator"
-export IOS_RELEASE_SCHEME="TestGame"
 export IOS_CARGO_PROFILE="release"
 export PROJECT_NAME="TestGame"
 export APP_NAME="test-game"
