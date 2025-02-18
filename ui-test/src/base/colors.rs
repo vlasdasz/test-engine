a9eed3404254be9f433fdbd4c7d6f06a618eb79b
Vladas Zakrevskis <146100@gmail.com>
2025-02-18 14:37:04 +0200

    colors test

--- ui-test/src/base/colors.rs
+++ ui-test/src/base/colors.rs
@@ -2,15 +2,21 @@ use anyhow::Result;
 use log::debug;
 use test_engine::{
     refs::Weak,
-    ui::{Setup, UI, view},
+    ui::{ImageView, Setup, UI, ViewData, view},
     ui_test::record_ui_test,
 };
 
 #[view]
-struct ColorsTestView {}
+struct ColorsTestView {
+    #[init]
+    image: ImageView,
+}
 
 impl Setup for ColorsTestView {
-    fn setup(self: Weak<Self>) {}
+    fn setup(mut self: Weak<Self>) {
+        self.image.place().tl(20).size(400, 520);
+        self.image.set_image("colors.png");
+    }
 }
 
 pub async fn test_colors() -> Result<()> {
