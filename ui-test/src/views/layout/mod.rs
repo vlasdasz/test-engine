2f36ec404f57b59d21d4671be2397f23b80f8ddf
Vladas Zakrevskis <146100@gmail.com>
2025-02-26 21:55:32 +0200

    Min width and height constraint

--- ui-test/src/views/layout/mod.rs
+++ ui-test/src/views/layout/mod.rs
@@ -1,10 +1,13 @@
 use center_field::test_center_field;
 use tiling_layout::test_tiling_layout;
+use crate::views::layout::min_width::test_min_width;
 
 mod center_field;
 mod tiling_layout;
+mod min_width;
 
 pub async fn test_layout() -> anyhow::Result<()> {
+    test_min_width().await?;
     test_center_field().await?;
     test_tiling_layout().await?;
     Ok(())
