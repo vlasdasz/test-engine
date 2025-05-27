c48078cc8f04a98865f8c014c68814bccc63349e
vladas <vladas@vladass-MacBook-Pro.local>
2025-05-27 23:33:59 +0300

    wip

--- deps/render/src/data/mod.rs
+++ deps/render/src/data/mod.rs
@@ -2,10 +2,12 @@ mod path_data;
 mod rect_instance;
 mod rect_view;
 mod ui_gradient_instance;
+mod ui_image_instance;
 mod ui_rect_instance;
 
 pub use path_data::*;
 pub use rect_instance::*;
 pub use rect_view::*;
 pub use ui_gradient_instance::*;
+pub use ui_image_instance::*;
 pub use ui_rect_instance::*;
---
+++ deps/render/src/data/ui_image_instance.rs
@@ -0,0 +1,40 @@
+use bytemuck::{Pod, Zeroable};
+use gm::{
+    color::Color,
+    flat::{Point, Rect, Size},
+};
+use wgpu::{BufferAddress, VertexBufferLayout, VertexStepMode};
+
+use crate::vertex_layout::VertexLayout;
+
+#[repr(C)]
+#[derive(Debug, Copy, Clone, Zeroable, Pod)]
+pub struct UIImageInstance {
+    pub position:      Point,
+    pub size:          Size,
+    pub corner_radius: f32,
+    pub z_position:    f32,
+    pub flip:          u32,
+}
+
+impl UIImageInstance {
+    pub fn new(rect: Rect, corner_radius: f32, z_position: f32) -> Self {
+        Self {
+            position: rect.origin,
+            size: rect.size,
+            corner_radius,
+            z_position,
+            flip: 0,
+        }
+    }
+}
+
+impl VertexLayout for UIImageInstance {
+    const ATTRIBS: &'static [wgpu::VertexAttribute] =
+        &wgpu::vertex_attr_array![2 => Float32x2, 3 => Float32x2, 4 => Float32x4, 5 => Float32, 6 => Float32];
+    const VERTEX_LAYOUT: VertexBufferLayout<'static> = VertexBufferLayout {
+        array_stride: size_of::<Self>() as BufferAddress,
+        step_mode:    VertexStepMode::Instance,
+        attributes:   Self::ATTRIBS,
+    };
+}
