b17b607cf217e90d1ba6c4b40641ecbd234ebde9
Vladas Zakrevskis <146100@gmail.com>
2025-06-08 14:41:09 +0300

    wip

--- deps/debug/src/lib.rs
+++ deps/debug/src/lib.rs
@@ -1,7 +1,15 @@
 use anyhow::Result;
 use log::info;
 use serde::{Deserialize, Serialize};
-use tokio::{io::AsyncReadExt, net::TcpListener, spawn, sync::mpsc::channel};
+use tokio::{
+    io::AsyncReadExt,
+    net::TcpListener,
+    spawn,
+    sync::{Mutex, mpsc::channel},
+};
+use vents::Event;
+
+static EVENT: Mutex<Event<MyMessage>> = Mutex::const_new(Event::const_default());
 
 #[derive(Serialize, Deserialize, Debug)]
 struct MyMessage {
@@ -17,7 +25,17 @@ async fn start_listening_internal() -> Result<()> {
     let listener = TcpListener::bind("127.0.0.1:4000").await?;
     info!("Server listening on 127.0.0.1:4000");
 
-    let (se, re) = channel::<MyMessage>(1);
+    let (se, mut re) = channel::<MyMessage>(1);
+
+    spawn(async move {
+        loop {
+            let Some(message) = re.recv().await else {
+                dbg!("nothing");
+                continue;
+            };
+            EVENT.lock().await.trigger(message);
+        }
+    });
 
     loop {
         let (mut socket, _) = listener.accept().await?;
@@ -31,5 +49,6 @@ async fn start_listening_internal() -> Result<()> {
         let json_str = std::str::from_utf8(&buf[..n])?;
         let msg: MyMessage = serde_json::from_str(json_str)?;
         info!("Received: {:?}", msg);
+        se.send(msg).await?;
     }
 }
