6c0dc31a4d5b9298ec95388753f83f146f0db1e6
Vladas Zakrevskis <146100@gmail.com>
2026-02-11 18:45:13 +0200

    wip

--- test-game/src/interface/test_game_view/menu_view.rs
+++ test-game/src/interface/test_game_view/menu_view.rs
@@ -1,7 +1,15 @@
+use anyhow::Result;
 use test_engine::{
     refs::Weak,
-    ui::{Setup, TableData, TableView, ViewData, view_test},
+    ui::{Setup, TableData, TableView, ViewData, ViewTest, view_test},
 };
+
+#[derive(Debug)]
+struct Node {
+    open:  bool,
+    value: String,
+    leafs: Vec<Node>,
+}
 
 #[view_test]
 pub struct MenuView {
@@ -20,7 +28,17 @@ impl TableData for MenuView {
         110.0
     }
 
+    fn variable_height(self: Weak<Self>) -> bool {
+        true
+    }
+
     fn number_of_cells(self: Weak<Self>) -> usize {
         5
+    }
+}
+
+impl ViewTest for MenuView {
+    fn perform_test(_view: Weak<Self>) -> Result<()> {
+        Ok(())
     }
 }
--- test-game/src/interface/test_game_view/mod.rs
+++ test-game/src/interface/test_game_view/mod.rs
@@ -1,5 +1,7 @@
 mod test_game_view;
 mod menu_view;
+mod node;
 
+pub use node::*;
 pub use menu_view::*;
 pub use test_game_view::*;
