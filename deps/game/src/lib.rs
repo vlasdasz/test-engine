d2cf7317507105006f52275b167e357e41b98735
Vladas Zakrevskis <146100@gmail.com>
2026-02-21 15:53:44 +0200

    checking gltf

--- deps/game/src/lib.rs
+++ deps/game/src/lib.rs
@@ -13,3 +13,33 @@ pub use game::*;
 pub use object::*;
 pub use primitives::*;
 pub use shape::*;
+
+#[cfg(test)]
+mod test {
+
+    use anyhow::Result;
+    use gltf::Gltf;
+
+    #[test]
+    #[ignore]
+    fn test_model_import() -> Result<()> {
+        let gltf = Gltf::open("/Users/vladas/Downloads/Untitled.gltf")?;
+
+        dbg!(gltf.scenes().len());
+        
+        for scene in gltf.scenes() {
+            for node in scene.nodes() {
+                println!("Node #{} has {} children", node.index(), node.children().count(),);
+            }
+        }
+        
+        let (document, buffers, images) = gltf::import("/Users/vladas/Downloads/Untitled.gltf")?;
+        
+        dbg!(document.meshes().len());
+        
+        assert_eq!(buffers.len(), document.buffers().count());
+        assert_eq!(images.len(), document.images().count());
+
+        Ok(())
+    }
+}
